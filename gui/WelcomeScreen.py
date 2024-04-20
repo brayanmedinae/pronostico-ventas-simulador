4291f22b0a1d94fa78d00909d55c81f466c48fea
Brayan Medina <brayanmedinae@outlook.com>
2024-04-19 21:03:28 -0600

    Add logo

--- gui/WelcomeScreen.py
+++ gui/WelcomeScreen.py
@@ -1,21 +1,26 @@
-from tkinter import ttk
+import customtkinter as ctk
 import gui.ConfigurationScreen
+from PIL import Image, ImageTk
 
-class WelcomeScreen(ttk.Frame):
+class WelcomeScreen(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
         self.pack(expand=True, fill="both")
         self.create_widgets()
 
     def create_widgets(self):
-        label = ttk.Label(self, text="Bienvenido", font="Helvetica 16 bold")
-        label.pack(pady=10)
-        start_button = ttk.Button(self, text="Iniciar simulación", command=lambda: self.go_to(gui.ConfigurationScreen.ConfigurationScreen))
-        start_button.pack(pady=10)
-        help_button = ttk.Button(self, text="Ayuda", command=lambda: print("Ayuda"))
-        help_button.pack(pady=10)
-        about_button = ttk.Button(self, text="Acerca de", command=lambda: print("Acerca de"))
-        about_button.pack(pady=10)
+        label = ctk.CTkLabel(self, text="Bienvenido", font=("Helvetica", 24))
+        label.pack(padx=20, pady=20)
+        start_button = ctk.CTkButton(self, text="Iniciar simulación", command=lambda: self.go_to(gui.ConfigurationScreen.ConfigurationScreen))
+        logo = Image.open("logo.png").resize((100, 100))
+        logo = ImageTk.PhotoImage(logo)
+        logo_label = ctk.CTkLabel(self, image=logo, text="")
+        logo_label.pack(padx=20, pady=20)
+        start_button.pack(padx=20, pady=20)
+        help_button = ctk.CTkButton(self, text="Ayuda", command=lambda: print("Ayuda"))
+        help_button.pack(padx=20, pady=20)
+        about_button = ctk.CTkButton(self, text="Acerca de", command=lambda: print("Acerca de"))
+        about_button.pack(padx=20, pady=20)
 
     def go_to(self, frame):
         self.destroy()
